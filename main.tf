44547d2ccb9fa9b047f31fe56ccf413f0aafbf17
Tan Ly <tientan0912@gmail.com>
2023-12-23 21:12:34 +0700

    update values

--- main.tf
+++ main.tf
@@ -3,6 +3,8 @@
 
 module "aft" {
   source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+  
+  # Required Vars
   ct_management_account_id    = var.ct_management_account_id
   log_archive_account_id      = var.log_archive_account_id
   audit_account_id            = var.audit_account_id
@@ -10,9 +12,15 @@ module "aft" {
   ct_home_region              = var.ct_home_region
   tf_backend_secondary_region = var.tf_backend_secondary_region
 
+  # VCS Vars
   vcs_provider                                  = "github"
   account_request_repo_name                     = "${var.github_username}/learn-terraform-aft-account-request"
   account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
   global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
   account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations"
+
+  # AFT Feature flags
+  aft_feature_cloudtrail_data_events      = false
+  aft_feature_enterprise_support          = false
+  aft_feature_delete_default_vpcs_enabled = true
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,10 +1,10 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-ct_management_account_id    = ""
-log_archive_account_id      = ""
-audit_account_id            = ""
-aft_management_account_id   = ""
-github_username             = ""
-ct_home_region              = ""
-tf_backend_secondary_region = ""
+ct_management_account_id    = "908271895989"
+log_archive_account_id      = "507561068062"
+audit_account_id            = "344225263533"
+aft_management_account_id   = "251711447124"
+github_username             = "tedly-rae"
+ct_home_region              = "ap-southeast-1"
+tf_backend_secondary_region = "ap-northeast-1"
